eaba1bd5cefbb043d9224fe3a143a32d1b624e6c
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-19 00:30:34 -0400

    concise solution that has horrible tiem complexity

--- arrays/minimumSwaps2.js
+++ arrays/minimumSwaps2.js
@@ -1,29 +1,24 @@
-let array = [4, 3, 1, 2, 0];
+let array = [1,3,5,2,4,6,7];
 
 function minimumSwaps(arr) {
   let swapCount = 0;
   let swap = (arr, index1, index2) => {
     [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
   };
-  for (let i = 0; i < arr.length; i++) {
-    let swapIndex;
-    let lowestNum = Infinity;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] <= arr[i] && arr[j] < lowestNum) {
-        lowestNum = arr[j];
-        swapIndex = j;
-      }
-      if (j === arr.length - 1) {
-        if (swapIndex !== i) {
-          swap(arr, swapIndex, i);
-          swapCount++;
-        }
-      }
+  const sortedArr = arr.slice().sort((a, b) => a - b);
+  let firstIndex = 0;
+  
+  while(JSON.stringify(arr) != JSON.stringify(sortedArr)){
+    const currentNum = arr[firstIndex];
+    if(currentNum !== firstIndex + 1){
+      swap(arr, firstIndex, currentNum - 1);
+      swapCount++;
     }
   }
+  
   return swapCount;
 }
 
-console.log(minimumSwaps(array));
+minimumSwaps(array);
 
 // found out that the solution will time out regardless for 8 - 13 on hacker rank
